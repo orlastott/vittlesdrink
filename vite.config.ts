93ef72dfe85ebb43eef71cd37bacdad1770963d4
orlastott <132671564+orlastott@users.noreply.github.com>
2026-02-06 16:25:28 +0000

    fixing live site

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,21 @@
 import { fileURLToPath } from "url";
 import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// Fix __dirname in ES module
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 export default defineConfig({
   root: path.resolve(__dirname, "client"),
   plugins: [react()],
+  build: {
+    outDir: path.resolve(__dirname, "dist", "public"),
+    emptyOutDir: true,
+  },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "client/src"),
-      "@shared": path.resolve(__dirname, "shared"),
+      "@": path.resolve(__dirname, "client", "src"),
     },
-  },
-  build: {
-    outDir: path.resolve(__dirname, "client/dist"),
-    emptyOutDir: true,
   },
 });
