5b483e20fcd7ab0b78f51ed24f88b89423be4a8c
orlastott <132671564+orlastott@users.noreply.github.com>
2026-02-06 13:37:19 +0000

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,16 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
+  root: path.resolve(__dirname, "client"),
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "client/src"),
       "@shared": path.resolve(__dirname, "shared"),
-      "@assets": path.resolve(__dirname, "attached_assets"),
     },
   },
-  root: path.resolve(__dirname, "client"),
   build: {
-    outDir: path.resolve(__dirname, "client/dist"), // <-- Vercel friendly
+    outDir: path.resolve(__dirname, "client/dist"),
     emptyOutDir: true,
   },
 });
